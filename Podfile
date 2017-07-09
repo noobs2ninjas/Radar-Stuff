7f813bb3c9c9db6b3d5c36f87ba42b5ada00eadb
Nathan Kellert <nathankellert@gmail.com>
2017-07-09 00:14:37 -0500

    Finishing MKMapView

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RadarStuff' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'GoogleMaps'
+  # Pods for RadarStuff
+
+  target 'RadarStuffTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RadarStuffUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- RadarStuff/ViewController.swift
+++ RadarStuff/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    var mapView: MKMapView!
+    var parser: KMLParser!
+    var overlays: [MKOverlay]!
+    var annotations: [MKAnnotation]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let location = CLLocationCoordinate2D(latitude: 35.4676, longitude: -97.5164)
+        let mapViewFrame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        mapView = MKMapView(frame: mapViewFrame)
+        mapView.region = MKCoordinateRegionMake(location, MKCoordinateSpanMake(1.0, 1.0))
+        mapView.centerCoordinate = location
+
+        if let path = Bundle.main.path(forResource: "example3", ofType: "kml"){
+            let url = URL(fileURLWithPath: path)
+            parser = KMLParser(url: url)
+            parser.parseKML()
+
+            overlays = parser.overlays
+            annotations = parser.points
+
+        } else {
+            print("not found")
+        }
+        mapView.delegate = self
+        self.view.addSubview(mapView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: MKMapViewDelegate{
 
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        return parser.viewForAnnotation(annotation)
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        return parser.rendererForOverlay(overlay)!
+    }
+
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        mapView.addOverlays(overlays)
+        mapView.addAnnotations(annotations)
+    }
 }
 
